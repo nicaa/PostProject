db33de406dca3ebc15fcbc0cf733d976ffadf8de
Nichlas Andreasen <nica1408@gmail.com>
2015-02-23 13:03:28 +0100

    sync

--- NameGenApp/NameGenApp/Form1.Designer.cs
+++ NameGenApp/NameGenApp/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-<<<<<<< HEAD
             this.button3 = new System.Windows.Forms.Button();
             this.fName = new System.Windows.Forms.TextBox();
             this.lName = new System.Windows.Forms.TextBox();
@@ -38,11 +37,8 @@
             this.button4 = new System.Windows.Forms.Button();
             this.IDText = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
             this.GenRandomButton = new System.Windows.Forms.Button();
-=======
             this.button2 = new System.Windows.Forms.Button();
->>>>>>> origin/master
             this.SuspendLayout();
             // 
             // button1
@@ -55,24 +51,61 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // button3
             // 
-<<<<<<< HEAD
-            this.button5.Location = new System.Drawing.Point(12, 91);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 11;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-=======
-            this.button2.Location = new System.Drawing.Point(83, 84);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Send Package";
-            this.button2.UseVisualStyleBackColor = true;
->>>>>>> origin/master
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            // 
+            // fName
+            // 
+            this.fName.Location = new System.Drawing.Point(0, 0);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(100, 20);
+            this.fName.TabIndex = 19;
+            // 
+            // lName
+            // 
+            this.lName.Location = new System.Drawing.Point(0, 0);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(100, 20);
+            this.lName.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            // 
+            // IDText
+            // 
+            this.IDText.Location = new System.Drawing.Point(0, 0);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(100, 20);
+            this.IDText.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 13;
             // 
             // GenRandomButton
             // 
@@ -84,14 +117,21 @@
             this.GenRandomButton.UseVisualStyleBackColor = true;
             this.GenRandomButton.Click += new System.EventHandler(this.GenRandomButton_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send Package";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(292, 425);
+            this.ClientSize = new System.Drawing.Size(292, 146);
             this.Controls.Add(this.GenRandomButton);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.IDText);
             this.Controls.Add(this.button4);
@@ -100,23 +140,20 @@
             this.Controls.Add(this.lName);
             this.Controls.Add(this.fName);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-=======
-            this.ClientSize = new System.Drawing.Size(292, 146);
->>>>>>> origin/master
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-<<<<<<< HEAD
+
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox fName;
         private System.Windows.Forms.TextBox lName;
@@ -125,12 +162,11 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox IDText;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button GenRandomButton;
-=======
+
         private System.Windows.Forms.Button button2;
 
->>>>>>> origin/master
+
 
     }
 }
